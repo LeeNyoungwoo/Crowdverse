3aacc70d1f67c672dc5f59a25cedef4afa8bd530
Nyoungwoo <leenw2@kaist.ac.kr>
2018-11-20 19:55:21 +0900

    fixing the perspective page

--- src/components/perspectivepage/EventBoxListItem/EventBoxListItem.js
+++ src/components/perspectivepage/EventBoxListItem/EventBoxListItem.js
@@ -7,24 +7,38 @@ class EventBoxListItem extends Component {
 
 		return (
 			<div class="eventBox">
-				<div class="eventImageBox">
-		   		<img src={data.imageName} class="eventImage"/>
+				<div class="eventUpperBox">
+					<img src={data.imageName} class="eventUpperImageBox"/>
+					<div class="eventUpperRightBox">
+						<div class="eventUpperRightTitleBox"><span class="spanStyle">{data.title}</span></div>
+					</div>
 				</div>
-				<div class="eventContentBox">
-					<div class="eventTitleBox">
-						<div class="eventContentTitle">{data.title}</div>
-					</div>
-					<div class="eventContentBox">
-						<div class="eventContentContent">{data.content}</div>
-					</div>
-					<div class="eventFoldBox">
-						<div class="eventFoldText">Show national diversity in this event</div>
-					</div>
+				<div class="eventLowerBox">
+					<div class="eventLowerContentBox">{data.content}</div>
 				</div>
 			</div>
+
 		);
 	}
 }
 
 export default EventBoxListItem;
 
+
+
+			// <div class="eventBox">
+			// 	<div class="eventImageBox">
+		 //   		<img src={data.imageName} class="eventImage"/>
+			// 	</div>
+			// 	<div class="eventContentBox">
+			// 		<div class="eventTitleBox">
+			// 			<div class="eventContentTitle">{data.title}</div>
+			// 		</div>
+			// 		<div class="eventContentcontentBox">
+			// 			<div class="eventContentContent">{data.content}</div>
+			// 		</div>
+			// 		<div class="eventFoldBox">
+			// 			<div class="eventFoldText">Show national diversity in this event</div>
+			// 		</div>
+			// 	</div>
+			// </div>
