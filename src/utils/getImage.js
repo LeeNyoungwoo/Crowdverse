407b1cd60262b4bda7dfd2f56dac8068775e8d97
Nyoungwoo <leenw2@kaist.ac.kr>
2018-12-05 20:06:13 +0900

    add picture

--- src/utils/getImage.js
+++ src/utils/getImage.js
@@ -4,6 +4,10 @@ import source2 from './source2.jpg';
 import source3 from './source3.jpg';
 import source4 from './source4.jpg';
 import source5 from './source5.jpg';
+import source6 from './source6.jpg';
+import source7 from './source7.jpg';
+import source8 from './source8.jpg';
+import source9 from './source9.jpg';
 import else1 from './else1.jpg';
 
 const getImage = (imageSrc) => {
@@ -13,6 +17,10 @@ const getImage = (imageSrc) => {
 	else if (imageSrc === "source3") {return source3}
 	else if (imageSrc === "source4") {return source4}
 	else if (imageSrc === "source5") {return source5}
+	else if (imageSrc === "source6") {return source6}
+	else if (imageSrc === "source7") {return source7}
+	else if (imageSrc === "source8") {return source8}
+	else if (imageSrc === "source9") {return source9}
 	else if (imageSrc === "individual") {return null}
 	else {return else1}
 }
